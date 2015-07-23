15425a74430a8a4ab60a785aef58c32959c542d1
John Matzen <john@ayxia.com>
2015-07-22 23:47:54 -0500

    build command line option changes
    Change-Id: Iee9bd51e2bd53cb46a0cf0c1acacb14187a1ef7c

---
+++ conf.bat
@@ -0,0 +1,33 @@
+configure ^
+	-opensource ^
+	-nomake examples ^
+	-nomake tests ^
+	-confirm-license ^
+	-c++11 ^
+	-opengl desktop ^
+	-no-angle ^
+	-no-sql-sqlite ^
+	-mp ^
+	-debug-and-release ^
+	-platform win32-msvc2013 ^
+	-prefix "msvc2013_out" ^
+	-skip qt3d ^
+	-skip qtcanvas3d ^
+	-skip qtmultimedia ^
+	-skip qtpim ^
+	-skip qtqa ^
+	-skip qtquick1 ^
+	-skip qtquickcontrols ^
+	-skip qtscript ^
+	-skip qtsensors ^
+	-skip qtserialport ^
+	-skip qtsvg ^
+	-skip qtwebchannel ^
+	-skip qtwebengine ^
+	-skip qtwebkit ^
+	-skip qtwebkit-examples ^
+	-skip qtwebsockets ^
+	-skip qttools ^
+	-skip qtdeclarative ^
+	-skip qtlocation
+	
